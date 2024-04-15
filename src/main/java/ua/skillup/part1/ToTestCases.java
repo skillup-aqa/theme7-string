824b280cb2cde254e069a2a3e2f94004c49ff2fd
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-15 17:57:50 +0300

    StringsSorter resolving

--- src/main/java/ua/skillup/part1/ToTestCases.java
+++ src/main/java/ua/skillup/part1/ToTestCases.java
@@ -2,7 +2,8 @@ package ua.skillup.part1;
 
 public class ToTestCases {
     public static void main(String[] args) {
-        PalindromeChecker toCheckPalindrome = new PalindromeChecker();
+        String[] testArray = {"mama", "Mama", "zorro", "papa", "archer", "Papa", "diamond", "01 point"};
         System.out.println(PalindromeChecker.isPalindrome("A man, a plan, a canal, Panama!"));
+        System.out.printf(String.join(", ", StringsSorter.sort(testArray)));
     }
 }
