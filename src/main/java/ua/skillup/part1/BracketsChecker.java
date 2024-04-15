e585cae666e63fbd79dd7c9fa736188a7821e965
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-15 18:27:23 +0300

    BracketsChecker resolving

--- src/main/java/ua/skillup/part1/BracketsChecker.java
+++ src/main/java/ua/skillup/part1/BracketsChecker.java
@@ -2,6 +2,15 @@ package ua.skillup.part1;
 
 public class BracketsChecker {
     public static boolean hasCorrectBrackets(String str) {
-        return false;
+        int bracketCounter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                bracketCounter++;
+            }
+            if (str.charAt(i) == ')') {
+                bracketCounter--;
+            }
+        }
+        return bracketCounter == 0;
     }
 }
--- src/main/java/ua/skillup/part1/ToTestCases.java
+++ src/main/java/ua/skillup/part1/ToTestCases.java
@@ -3,7 +3,8 @@ package ua.skillup.part1;
 public class ToTestCases {
     public static void main(String[] args) {
         String[] testArray = {"mama", "Mama", "zorro", "papa", "archer", "Papa", "diamond", "01 point"};
-        System.out.println(PalindromeChecker.isPalindrome("A man, a plan, a canal, Panama!"));
-        System.out.printf(String.join(", ", StringsSorter.sort(testArray)));
+        System.out.println("Palindrome checker: " + PalindromeChecker.isPalindrome("A man, a plan, a canal, Panama!"));
+        System.out.println("Sorting string array: " + String.join(", ", StringsSorter.sort(testArray)));
+        System.out.println("Checking brackets: " + BracketsChecker.hasCorrectBrackets("(some(value))"));
     }
 }
