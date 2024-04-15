b62e466a2c5cf48970f97179d7d04b43865a62c5
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-15 15:26:35 +0300

    PalindromeChecker resolving and ToTestCases added

--- src/main/java/ua/skillup/part1/PalindromeChecker.java
+++ src/main/java/ua/skillup/part1/PalindromeChecker.java
@@ -2,6 +2,14 @@ package ua.skillup.part1;
 
 public class PalindromeChecker {
     public static boolean isPalindrome(String str) {
-        return false;
+        StringBuilder lettersOnly = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c)) {
+                lettersOnly.append(c);
+            }
+        }
+        str = lettersOnly.toString();
+        return str.equalsIgnoreCase(lettersOnly.reverse().toString());
     }
 }
---
+++ src/main/java/ua/skillup/part1/ToTestCases.java
@@ -0,0 +1,8 @@
+package ua.skillup.part1;
+
+public class ToTestCases {
+    public static void main(String[] args) {
+        PalindromeChecker toCheckPalindrome = new PalindromeChecker();
+        System.out.println(PalindromeChecker.isPalindrome("A man, a plan, a canal, Panama!"));
+    }
+}
