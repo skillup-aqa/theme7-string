71015b0d2de1d3b4ee85bd96b16bd10c618eb7ee
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-15 22:32:06 +0300

    AtbashCypher resolving

--- src/main/java/ua/skillup/part1/AtbashCypher.java
+++ src/main/java/ua/skillup/part1/AtbashCypher.java
@@ -1,11 +1,25 @@
 package ua.skillup.part1;
 
+import java.awt.*;
+
 public class AtbashCypher {
+    static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
     public static String encode(String str) {
-        return null;
+        char[] stringArray = str.toLowerCase().toCharArray();
+        char[] encodedArray = new char[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            if (ALPHABET.indexOf(stringArray[i]) != -1) {
+                encodedArray[i] = ALPHABET.charAt(ALPHABET.length() - ALPHABET.indexOf(stringArray[i]) - 1);
+            } else {
+                encodedArray[i] = stringArray[i];
+            }
+
+        }
+        return new String(encodedArray);
     }
 
     public static String decode(String str) {
-        return null;
+        return encode(str);
     }
 }
