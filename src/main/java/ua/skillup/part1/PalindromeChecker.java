1d1f5a768e991431903a36c123f10f35e990ffd9
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-21 09:40:24 -0400

    Resolving Palindrome Checker

--- src/main/java/ua/skillup/part1/PalindromeChecker.java
+++ src/main/java/ua/skillup/part1/PalindromeChecker.java
@@ -2,6 +2,24 @@ package ua.skillup.part1;
 
 public class PalindromeChecker {
     public static boolean isPalindrome(String str) {
-        return false;
+        StringBuilder letters = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char a = str.charAt(i);
+            if (Character.isLetter(a)) {
+                letters.append(a);
+            }
+        }
+        str = letters.toString();
+        return str.equalsIgnoreCase(letters.reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        // PalindromeChecker Checker = new PalindromeChecker();
+        String str = "Mama";
+        if (isPalindrome(str)) {
+            System.out.println("The string '" + str + "' is a palindrome");
+        } else {
+            System.out.println("The string '" + str + "' not palindrome");
+        }
     }
 }
