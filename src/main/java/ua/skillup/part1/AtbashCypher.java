ba8b9d4161cd341e87bc95213973b3cc0763b5d2
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-21 11:06:18 -0400

    Resolving Atbash Cypher

--- src/main/java/ua/skillup/part1/AtbashCypher.java
+++ src/main/java/ua/skillup/part1/AtbashCypher.java
@@ -1,11 +1,24 @@
 package ua.skillup.part1;
 
 public class AtbashCypher {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static String encode(String str) {
-        return null;
+       char[] letters = str.toLowerCase().toCharArray();
+       char[] encode = new char[letters.length];
+       for (int i = 0; i < letters.length; i++){
+           if (ALPHABET.indexOf(letters[i]) != -1) {
+               encode[i] = ALPHABET.charAt(ALPHABET.length() - ALPHABET.indexOf(letters[i]) - 1);
+           }else {
+               encode[i] = letters[i];
+           }
+       }
+        return new String(encode);
     }
 
     public static String decode(String str) {
-        return null;
+        return encode(str);
+    }
+    public static void main(String[] args) {
+        System.out.println("Atbash encode - " + AtbashCypher.encode("abcdefghijklmnopqrstuvwxyz + Tati"));
     }
 }
