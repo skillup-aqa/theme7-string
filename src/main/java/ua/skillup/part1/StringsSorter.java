029fc7e0fedd5584be1c21bef7c23615de8692c9
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-21 10:48:14 -0400

    Resolving Strings Sorter

--- src/main/java/ua/skillup/part1/StringsSorter.java
+++ src/main/java/ua/skillup/part1/StringsSorter.java
@@ -1,7 +1,22 @@
 package ua.skillup.part1;
 
 public class StringsSorter {
-    public static String[] sort(String [] strArray) {
-        return null;
+    public static String[] sort(String[] strArray) {
+        String temp;
+        for (int i = 0; i < strArray.length; i++) {
+            for (int k = i + 1; k < strArray.length; k++) {
+                if (strArray[i].compareTo(strArray[k]) > 0) {
+                    temp = strArray[i];
+                    strArray[i] = strArray[k];
+                    strArray[k] = temp;
+                }
+            }
+        }
+        return strArray;
+    }
+
+    public static void main(String[] args) {
+        String[] exampleToSort = {"seven", "five", "two", "one", "six", "three", "four"};
+        System.out.println(String.join(",", StringsSorter.sort(exampleToSort)));
     }
 }
