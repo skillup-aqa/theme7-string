1807a79dcdee0ded14c8c3a6636f0b77a70e7b52
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-21 10:30:33 -0400

    Resolving Brackets Checker

--- src/main/java/ua/skillup/part1/BracketsChecker.java
+++ src/main/java/ua/skillup/part1/BracketsChecker.java
@@ -2,6 +2,21 @@ package ua.skillup.part1;
 
 public class BracketsChecker {
     public static boolean hasCorrectBrackets(String str) {
-        return false;
+        int bracketsChecker = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                bracketsChecker++;
+            }
+            if (str.charAt(i) == ')') {
+                bracketsChecker--;
+            }
+        }
+        return bracketsChecker == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Checking brackets - " + hasCorrectBrackets("example((to check)"));
+
+
     }
 }
